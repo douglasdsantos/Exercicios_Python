9bc3262b96b9983d4672b58ce29c57da8d15322f
douglasdsantos <douglasdie@gmail.com>
2017-07-02 14:26:21 -0300

    Lista Exercicios

---
+++ exe03.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite um outro valor: '))
+soma = n1 + n2
+print('A soma entre {} e {} é igual a {}.' .format(n1, n2, soma))
---
+++ exe05.py
@@ -0,0 +1,6 @@
+# Faça um Programa que converta metros para centímetros.
+
+metros = float(input('Digite a metragem que deseja converter para cm: '))
+cm = metros * 100
+
+print('{:.2f} metros equivale a {:.2f} centimetros. '.format(metros, cm))
