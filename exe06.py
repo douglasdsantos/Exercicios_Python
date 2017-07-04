75cec9815430926e97ae0689c608e5e7ff3af255
douglasdsantos <douglasdie@gmail.com>
2017-07-04 12:10:12 -0300

    Atualizando o exe 06

---
+++ exe06.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+pi = 3.14
+raio_circulo = float(input('Digite o raio do circulo: '))
+area_circulo = pi * (raio_circulo * raio_circulo)
+
+print('A área do circulo é e {:.2f}m². '.format(area_circulo))
